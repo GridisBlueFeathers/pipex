fa5272bc79d11acd4f117541f51a9acf2c7c6486
Serhii Veretennikov <gridis72@gmail.com>
2024-06-18 14:26:15 +0200

    feat: implemented basic functionality

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:00:26 by svereten          #+#    #+#             */
-/*   Updated: 2024/06/18 12:26:42 by svereten         ###   ########.fr       */
+/*   Updated: 2024/06/18 13:41:42 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -112,7 +112,6 @@ int	state_feed_process_commands(t_pipex_state *state)
 			return (0);
 		i++;
 	}
-	
 	return (1);
 }
 
@@ -131,48 +130,9 @@ int main(int argc, char **argv, char **envp) {
 	state_feed(&state);
 	if (state.error)
 		return (state_free(&state), 1);
-	state_free(&state);
 
-	/*int fd = open("./infile", O_WRONLY | O_CREAT);
-  dup2(fd, STDOUT_FILENO);
-  close(fd);
-  printf("hello there\n");
-  return (0);*/
+	int	fd[2];
 
-
-	/*int i = 0;
-	char *path_raw;
-	while (envp[i]) {
-		if (!ft_strncmp("PATH=", envp[i], 5)) {
-			path_raw = ft_substr(envp[i], 5, ft_strlen(envp[i]));
-			if (!path_raw) {
-				printf("death\n");
-				return (1);
-			}
-		}
-		i++;
-	}
-	char **pathes;
-	pathes = ft_split(path_raw, ':');
-	i = 0;
-	while (pathes[i]) {
-		pathes[i] = ft_strjoin(pathes[i], "/");
-		if (!access(ft_strjoin(ft_strdup(pathes[i]), "ls"), X_OK))
-			printf("%s\n", ft_strjoin(ft_strdup(pathes[i]), "ls"));
-		i++;
-	}*/
-
-	/*char *args_grep[] = {"grep", "hey", NULL};
-	char *args_wc[] = {"wc", "-l", NULL};
-	char *args_ls[] = {"ls", "-a", NULL};
-	(void)args_ls;
-	(void)args_grep;
-	(void)args_wc;
-
-	int	fd[2];*/
-
-	//if (access("./nofile", R_OK))
-	//	perror("Error");
 	/*int fd_infile = open("./nofile", O_RDONLY);
 	if (fd_infile == -1)
 		perror("Error");
@@ -183,14 +143,14 @@ int main(int argc, char **argv, char **envp) {
 		content = ft_strjoin(content, buf);
 		ft_free_n_null((void **)&buf);
 	}
-	close(fd_infile);
+	close(fd_infile);*/
 
 	pipe(fd);
 	pid_t p2 = fork();
 	if (!p2)
 	{
 		//write(fd[1], content, ft_strlen(content));
-		int fd_infile = open("./nofile", O_RDONLY);
+		int fd_infile = open("./infile", O_RDONLY);
 		if (fd_infile == -1)
 		{
 			close(fd[1]);
@@ -202,17 +162,31 @@ int main(int argc, char **argv, char **envp) {
 		close(fd_infile);
 		close(fd[1]);
 		close(fd[0]);
-		execve("/usr/bin/ls", args_ls, NULL);
+		execve(state.commands[0]->path, state.commands[0]->args, NULL);
 	}
+	/*else
+	{
+		close(fd[1]);
+		close(fd[0]);
+		exit (0);
+	}*/
 	pid_t p1 = fork();
 	if (!p1)
 	{
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[1]);
 		close(fd[0]);
-		execve("/usr/bin/wc", args_wc, NULL);
+		execve(state.commands[1]->path, state.commands[1]->args, NULL);
+	}
+	/*else
+	{
+		close(fd[1]);
+		close(fd[0]);
+		exit (0);
 	}*/
 
+	close(fd[1]);
+	close(fd[0]);
 	//write(0, content, ft_strlen(content));
 	//ft_free_n_null((void **)&content);
 	/*int	fd[2];
@@ -229,4 +203,5 @@ int main(int argc, char **argv, char **envp) {
 		close(fd[0]);
 		close(fd[1]);
 	}*/
+	state_free(&state);
 }
